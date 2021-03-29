f2ca9ad03c088b868b964442fc971b502de27758
Junior Gutierrez <jlgutierrez2000@hotmail.com>
2021-03-29 17:08:44 -0400

    fix more bugs yet

--- tcp_chat.c
+++ tcp_chat.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]){
         recv(network_socket, &server_reponse, sizeof(server_reponse), 0);
 
         //print the server message
-        printf("Server: %s", server_reponse);
+        //printf("Server: %s", server_reponse);
 
         close(network_socket);
     }else if (strcmp(argv[1], "-server") == 0){
@@ -48,24 +48,24 @@ int main(int argc, char *argv[]){
         //server_address.sin_addr.s_addr = INADDR_ANY;
 
         //create the socket
-        int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
         //bind the socket to our specified IP and port
-        bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)); //Igual que connect()
+        bind(network_socket, (struct sockaddr *)&server_address, sizeof(server_address)); //Igual que connect()
 
         //listening for connection
-        listen(server_socket, 5); //1 Parametro: ID del socket. 2 Parametro: cantidad de conexiones
+        listen(network_socket, 1); //1 Parametro: ID del socket. 2 Parametro: cantidad de conexiones
 
-        int client_socket = accept(server_socket, NULL, NULL); //INVESTIGAR MAS ESTO
+        int client_socket = accept(network_socket, NULL, NULL); //INVESTIGAR MAS ESTO
 
-        char server_message[256] = "HOLA";
-        send(client_socket, server_message, sizeof(server_message), 0); //Enviar mensaje desde el server al cliente
+        char buffer[256] = "Hello";
+        send(client_socket, buffer, sizeof(buffer), 0); //Enviar mensaje desde el server al cliente
 
         //close the socket
-        close(server_socket);
+        close(network_socket);
     }else{
         perror("ERROR: Especifica el modo");
         exit(EXIT_FAILURE);
     }
+    printf("Mensaje enviado, cerrando el server");
     return 0;
 }
