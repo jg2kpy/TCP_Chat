19aac4cc9f7c225e358c88202f5cec000dc63021
Junior Gutierrez <jlgutierrez2000@hotmail.com>
2021-03-29 12:53:05 -0400

    second commit

---
+++ tcp_chat.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+
+int main(int argc, char *argv[]){
+    if (argc != 2){
+        perror("ERROR: Especifica el modo");
+        exit(EXIT_FAILURE);
+    }
+
+    int network_socket = socket(AF_INET, SOCK_STREAM, 0); //SOCKET FOR TCP/IP
+
+    int port = 6969;
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);       //Specify the port
+    inet_pton(AF_INET, "192.168.0.10", &server_address.sin_addr);
+    //mi ip: "181.126.194.28"
+
+    if (strcmp(argv[1], "-client") == 0){
+
+        int connection_status = connect(network_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+
+        if (connection_status == -1){
+            perror("ERROR: connect()");
+        }
+
+        //recieve data from the server
+        char server_reponse[256];
+        recv(network_socket, &server_reponse, sizeof(server_reponse), 0);
+
+        //print the server message
+        printf("Server: %s", server_reponse);
+
+        close(network_socket);
+    }else if (strcmp(argv[1], "-server") == 0){
+
+        //server_address.sin_addr.s_addr = INADDR_ANY;
+
+        //create the socket
+        int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+        //bind the socket to our specified IP and port
+        bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)); //Igual que connect()
+
+        //listening for connection
+        listen(server_socket, 5); //1 Parametro: ID del socket. 2 Parametro: cantidad de conexiones
+
+        int client_socket = accept(server_socket, NULL, NULL); //INVESTIGAR MAS ESTO
+
+        char server_message[256] = "Hello!!";
+        send(client_socket, server_message, sizeof(server_message), 0); //Enviar mensaje desde el server al cliente
+
+        //close the socket
+        close(server_socket);
+    }else{
+        perror("ERROR: Especifica el modo");
+        exit(EXIT_FAILURE);
+    }
+    return 0;
+}
